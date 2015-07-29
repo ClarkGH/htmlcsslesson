a29f16d4386bdcd0ae4fafc4e32438336eb93287
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2015-07-29 16:41:59 -0700

    lesson is all set up

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 console.log("Hi, main.js has successfully been loaded into the page.");
 
+$(document).ready(function() {
+
+  $( "#target" ).click(function() {
+    alert( "Hey Macarena" );
+  });
+
+  // $( "#partial" ).load( "about.html #target" );  
+
+})
